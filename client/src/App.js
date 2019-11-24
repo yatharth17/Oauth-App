f051328b5680df78baf8731ba92b000b27ddf742
yatharth17 <yatharthkatyal4@gmail.com>
2019-11-24 21:49:25 +0530

    added react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my MERN app</h1>
+      <a href="/auth/google">Sign Up with Google</a>
+    </div>
+  );
+}
+
+export default App;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,4 +5,13 @@ module.exports=(app)=>{
     passport.authenticate('google', { scope: ['profile','email'] }));
   
     app.get('/auth/google/callback',passport.authenticate('google'))  
+
+    app.get('/api/current_user',(req,res)=>{
+        res.send(req.user);
+    })
+
+    app.get('/api/logout',(req,res)=>{
+        req.logout();
+        res.send(req.user);
+    })
 }
