8b2fad0a5c4a071ee3469265941299e6a869e10d
yatharth17 <yatharthkatyal4@gmail.com>
2019-11-24 16:21:34 +0530

    Google auth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleClientID:"952613184669-s0gftgjgk9c9d55qlo25rkcqua7goju7.apps.googleusercontent.com",
+    googleClientSecret:"W5w9Xr5YD_5gjAaS-_dUiHK6",
+    mongoURI: "mongodb+srv://Yath_17:frontech@mern-tbcbt.mongodb.net/test?retryWrites=true&w=majority",
+    cookieKey:"djvhdbvbjnvknvdnkdn"
+}
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express=require('express')
 const passport=require('passport')
 const GoogleStrategy =require('passport-google-oauth20')
-
+const keys=require("./config/keys")
 const app =express()
 const port =process.env.PORT || 5000
+const mongoose=require('mongoose')
+const cookie=require('cookie-session')
+require('./models/User')
+require('./services/passport')
 
-app.get('/', (req, res) => {
-    res.send("hi");
-    
-});
+mongoose.connect(keys.mongoURI,()=>{
+    console.log('MongoDB connected')
+})
+
+app.use(cookie({
+    maxAge:30*24*60*60*1000,
+    keys:[keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+require('./routes/authRoute')(app)
+
+
 
 app.listen(5000, () => {
     console.log('App listening on port 5000!');
